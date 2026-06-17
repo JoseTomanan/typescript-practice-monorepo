2cf31463560e1010e9566c81d85e7d1851ef020e
JoseTomanan <jdtomanan@up.edu.ph>
2026-06-17 18:23:07 +0800

    feat(nest-practice): add profiles module scaffold

---
+++ nest-practice/src/profiles/profiles.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+
+@Controller('profiles')
+export class ProfilesController {
+  // GET /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{ location }];
+  }
+
+  // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+}
