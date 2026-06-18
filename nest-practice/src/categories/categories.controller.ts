69ca0111106455562e8722b20b4744156c4fbb90
HoweZae <jdtomanan@up.edu.ph>
2026-06-18 12:54:06 +0800

    feat: Create `Categories` service; Update controller

--- nest-practice/src/categories/categories.controller.ts
+++ nest-practice/src/categories/categories.controller.ts
@@ -1,46 +1,41 @@
-import {
-  Controller,
-  Param,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Body,
-} from '@nestjs/common';
+import { Controller, Param, Get, Post, Put, Delete, Body, } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
 export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
   // GET /categories
   @Get()
   findAll() {
-    return [{ id: 1, name: 'Category 1' }, { id: 2, name: 'Category 2' }];
+    return this.categoriesService.findAll();
   }
 
   // GET /categories/:id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id, name: 'Category 1' };
+  findOne(@Param('id') id: number) {
+    return this.categoriesService.findOne(id);
   }
 
   // POST /categories
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
-    return { id: '3', name: createCategoryDto.name };
+    return this.categoriesService.create(createCategoryDto.name);
   }
 
   // PUT /categories/:id
   @Put(':id')
   update(
-    @Param('id') id: string,
+    @Param('id') id: number,
     @Body() updateCategoryDto: CreateCategoryDto,
   ) {
-    return { id, ...updateCategoryDto };
+    return this.categoriesService.update(id, updateCategoryDto.name);
   }
 
   // DELETE /categories/:id
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return { id };
+  remove(@Param('id') id: number) {
+    return this.categoriesService.remove(id);
   }
 }
--- nest-practice/src/categories/categories.service.ts
+++ nest-practice/src/categories/categories.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from './entities/category.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  private categories: Category[] = [
+    { id: 1, name: 'Category 1' },
+    { id: 2, name: 'Category 2' },
+  ];
+
+  findAll(): Category[] {
+    return this.categories;
+  }
+
+  findOne(id: number): Category | undefined {
+    return this.categories.find(category => category.id === id);
+  }
+
+  create(name: string): Category {
+    const newCategory = { id: this.categories.length + 1, name };
+    this.categories.push(newCategory);
+    return newCategory;
+  }
+
+  update(id: number, name: string): Category | null {
+    const categoryIndex = this.categories.findIndex(category => category.id === id);
+
+    if (categoryIndex === -1)
+      return null;
+
+    this.categories[categoryIndex].name = name;
+    return this.categories[categoryIndex];
+  }
+
+  remove(id: number): Category | null {
+    const categoryIndex = this.categories.findIndex(category => category.id === id);
+
+    if (categoryIndex === -1)
+      return null;
+    
+    const removedCategory = this.categories.splice(categoryIndex, 1);
+    return removedCategory[0];
+  }
+}
