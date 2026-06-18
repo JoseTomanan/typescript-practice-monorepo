98be73d6fe104e49a344c23a9650bfd9ccdc8192
HoweZae <jdtomanan@up.edu.ph>
2026-06-18 12:46:18 +0800

    feat: create product service, import to controller

--- nest-practice/src/products/products.controller.ts
+++ nest-practice/src/products/products.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   // GET /products
   @Get()
   findAll() {
-    return [{ name: 'Product 1' }, { name: 'Product 2' }];
+    return this.productsService.findAll();
   }
 
   // GET /products/:id
   @Get(':id')
-  findOne() {
-    return { name: 'Product 1' };
+  findOne(@Param('id') id: number) {
+    return this.productsService.findOne(id);
   }
 
   // POST /products
@@ -22,7 +25,7 @@ export class ProductsController {
 
   // PUT /products/:id
   @Put(':id')
-  update() {
+  update(@Param('id') id: number) {
     // Logic to update a product
   }
 
--- nest-practice/src/products/products.service.ts
+++ nest-practice/src/products/products.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './entities/product.entity';
+
+
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: "iPhone 15",
+      price: 59999,
+      stock: 10,
+      categoryId: 1
+    },
+    {
+      id: 2,
+      name: "Samsung S25",
+      price: 54999,
+      stock: 8,
+      categoryId: 1
+    },
+  ];
+  
+  findAll(): Product[] {
+    return this.products;
+  }
+
+  findOne(id: number): Product | undefined {
+    return this.products.find(product => product.id === id);
+  }
+
+  create(product: Omit<Product, 'id'>): Product {
+    const newProduct = { id: this.products.length + 1, ...product };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id: number, updatedProduct: Partial<Product>): Product | null {
+    const productIndex = this.products.findIndex(product => product.id === id);
+
+    if (productIndex === -1)
+      return null;
+
+    this.products[productIndex] = { ...this.products[productIndex], ...updatedProduct };
+    return this.products[productIndex];
+  }
+
+  remove(id: number): Product | null {
+    const productIndex = this.products.findIndex(product => product.id === id);
+
+    if (productIndex === -1)
+      return null;
+    
+    const removedProduct = this.products.splice(productIndex, 1);
+    return removedProduct[0];
+  }
+}
