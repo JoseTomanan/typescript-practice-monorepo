293da4de277724466b18012456b33cc8b9533b4f
JoseTomanan <jdtomanan@up.edu.ph>
2026-06-22 18:44:28 +0800

    refactor: Dissolve categories.service.ts into use cases, persistence

---
+++ nest-practice/src/categories/application/use-cases/create-category.use-case.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CATEGORY_REPOSITORY } from '../../domain/category.repository';
+import type { CategoryRepository } from '../../domain/category.repository';
+import { Category } from '../../domain/category.entity';
+
+@Injectable()
+export class CreateCategoryUseCase {
+  constructor(
+    @Inject(CATEGORY_REPOSITORY) private readonly categoryRepository: CategoryRepository,
+  ) {}
+
+  execute(name: string): Category {
+    const categories = this.categoryRepository.findAll();
+    const newCategory: Category = { id: categories.length + 1, name };
+
+    this.categoryRepository.save(newCategory);
+    return newCategory;
+  }
+}
