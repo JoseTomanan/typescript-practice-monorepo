f8ac296d260ada2b725afe50fb1e4efd0917f58b
JoseTomanan <jdtomanan@up.edu.ph>
2026-06-25 13:43:23 +0800

    docs: Spacing

--- nest-practice/src/categories/presentation/dto/update-category.dto.ts
+++ nest-practice/src/categories/presentation/dto/update-category.dto.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 export const UpdateCategorySchema = z.object({
-  name: z.string().min(1, { message: "Name must be at least 1 character long" }),
+  name: z.string()
+    .min(1, { message: "Name must be at least 1 character long" })
+    .max(255, { message: "Name must be at most 255 characters long" }),
 });
 
 export type UpdateCategoryDto = z.infer<typeof UpdateCategorySchema>;
