7525bece102654918e18c7e6a47a68a0a6c49d1d
HoweZae <jdtomanan@up.edu.ph>
2026-06-26 15:55:50 +0800

    refactor: replace handwritten ZodValidationPipe with simpler

--- nest-practice/src/app.module.ts
+++ nest-practice/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ZodValidationPipe } from './app.pipe';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [ProductsModule, CategoriesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
