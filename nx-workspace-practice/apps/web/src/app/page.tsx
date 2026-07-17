aeb1e4cc7c830800f378d1ed5a59f740e3238c9f
JoseTomanan <jdtomanan@up.edu.ph>
2026-07-17 12:38:56 +0800

    feat: Use new function fetchApi

--- nx-workspace-practice/apps/web/src/app/page.tsx
+++ nx-workspace-practice/apps/web/src/app/page.tsx
@@ -1,23 +1,21 @@
 import styles from './page.module.css';
 import { Message } from 'shared';
+import fetchApi from 'shared';
 
-async function getMessage(): Promise<Message> {
-  const response = await fetch('http://localhost:3000/api', {
+export default async function Index() {
+  const message: Message = await fetchApi('message', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
   });
-  return response.json();
-}
-
-export default async function Index() {
-  const message = await getMessage();  
 
   return (
     <div className={styles.page}>
       <h1>Welcome to web!</h1>
-      <p>{message.message}</p>
+      <p>Message from backend (placeholder): {message.message}</p>
+      <h2>TO DO LIST ITEMS</h2>
+      
     </div>
   );
 }
