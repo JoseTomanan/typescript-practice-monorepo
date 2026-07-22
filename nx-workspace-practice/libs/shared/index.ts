a66f1bf82ced13b019ac12d1e71b8a74683211af
JoseTomanan <jdtomanan@up.edu.ph>
2026-07-22 15:40:43 +0800

    refactor(shared): consolidate exports by moving index.ts to root folder

---
+++ nx-workspace-practice/libs/shared/index.ts
@@ -0,0 +1,11 @@
+export * from './domain/TodoItem';
+export type { TodoTableProps } from './domain/TodoTableProps';
+export * from './domain/Message';
+export * from './api-contracts/CreateTodoDto';
+export * from './api-contracts/UpdateTodoDto';
+export * from './api-contracts/UpdateTodoStatusDto';
+export * from './domain/constants/TodoStatusValues';
+
+export { default } from './src/utils/FetchAPI';
+export * from './src/utils/TodoAPI';
+export * from './src/utils/DateFormat';
